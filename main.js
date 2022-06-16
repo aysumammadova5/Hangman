9dbbf55758f8572f85f21d17fb1fdaae67d100f7
aysumammadova5 <aysumammadova554@gmail.com>
2022-06-16 15:38:05 +0400

    Hangman

---
+++ main.js
@@ -0,0 +1,145 @@
+var adam = new Array("___\n", "  |\n", " O\n", " /", "|", "\\\n", " /", " \\\n", "___");
+var kelimeler= ["QAPI","PƏNCƏRƏ","DƏRZİ","","SEYFƏVİLƏR","KONSULTASİYA","ASILQAN","LİMON"];
+var kelime;
+var hak=0;
+ 
+//nesnelerin oluşturulması
+var kelimeUret= document.getElementById("kelime-uret");
+var daragaci= document.getElementById("daracagi");
+var cikanHarf= document.getElementById("cikan-harf");
+var harfler= document.querySelectorAll(".harf");
+ 
+//bootstrap(otomatik çalışacak kodlar)
+(function(){
+harfler.forEach(function(gelen) {
+gelen.onclick=function(olay){
+ 
+this.setAttribute("disabled","disabled");
+var durum= harfKontrol(kelime,this.textContent);
+harfEkle(this.textContent);
+if(durum)
+{
+harfYerlestir(kelime,this.textContent);
+ 
+}
+else
+{
+daragaci.textContent+= adam[hak];
+hak++;
+}
+ 
+var tireDurum=tireKontrol();
+if(!tireDurum)
+{
+window.alert("TƏBRİKLƏR!");
+tumHarflerPasif();
+ 
+}
+if(adam.length<=hak)
+{
+window.alert("ÖLDÜNÜZ!");
+tumHarfleriYaz(kelime);
+tumHarflerPasif();
+ 
+}
+}
+});
+harfSec();
+ 
+})();
+ 
+kelimeUret.onclick=harfSec;
+ 
+//fonksiyalar
+ 
+function harfSec()
+{
+var sira=Math.round(Math.random()*kelimeler.length);
+kelime=new String(kelimeler[sira]);
+kelime=kelime.split("");
+ 
+/*kelime= kelime.map(function(gelen){
+return gelen+"_";
+});*/
+ 
+yertutucu(kelime);
+}
+ 
+//gizli herflerin yerine gösterilecek butonlar
+function yertutucu(kelime)
+{
+var yertutucu=document.getElementById("yertutucu");
+yertutucu.innerHTML="";
+daragaci.innerHTML="";
+tumHarflerAktif();
+hak=0;
+ 
+for(var i=0;i<kelime.length;i++)
+{
+var harf = document.createElement("button");
+harf.setAttribute("type","button")
+harf.classList.add("btn", "btn-primary", "bharf");
+harf.textContent="_";
+yertutucu.appendChild(harf);
+}
+}
+ 
+function harfKontrol(kelime,harf){
+return kelime.some(x => x ==harf );
+}
+ 
+function harfYerlestir(kelime,harf){
+var bharfler= document.querySelectorAll(".bharf");
+for(sira in kelime)
+{
+if(harf==kelime[sira])
+{
+bharfler[sira].textContent=harf;
+}
+}
+}
+ 
+function tumHarfleriYaz(kelime)
+{
+var bharfler= document.querySelectorAll(".bharf");
+for(sira in kelime)
+{
+harfYerlestir(kelime,kelime[sira]);
+}
+}
+ 
+function tumHarflerPasif()
+{
+harfler.forEach(function(eleman){
+eleman.setAttribute("disabled","disabled");
+});
+ 
+}
+function tumHarflerAktif()
+{
+harfler.forEach(function(eleman){
+eleman.removeAttribute("disabled");
+});
+ 
+}
+function tireKontrol()
+{
+var durum= false;
+var bharfler= document.querySelectorAll(".bharf");
+ 
+for(sira in bharfler)
+{
+ 
+if(bharfler[sira].textContent=="_")
+{
+durum=true;
+}
+}
+return durum;
+ 
+}
+ 
+function harfEkle(harf)
+{
+cikanHarf.textContent=cikanHarf.textContent+" "+harf;
+}
